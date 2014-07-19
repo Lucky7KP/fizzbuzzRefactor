4056ea51721709786b31f7e47cc7114625e63de4
Lucky7KP <kpeltier@Kristinas-MacBook-Pro.local>
2014-07-18 23:50:43 -0700

    add html code

--- js/app.js
+++ js/app.js
@@ -1,16 +1,14 @@
 $(document).ready(function(){
-    for(var counter = 1; counter <=100; counter++){
+
+    for(var counter = 1; counter <=100; counter++) {
     	if (counter % 3 === 0 && counter % 5 === 0) {
 			$("#main").append("<p>FizzBuzz</p>");
-		} 
-		else if (counter % 3 === 0) {
+		}  else if (counter % 3 === 0) {
 			$("#main").append("<p>Fizz</p>");
-		} 
-		else if (counter % 5 === 0) {
+		}  else if (counter % 5 === 0) {
 			$("#main").append("<p>Buzz</p>");
-		}
-		else {
+		}  else {
 	    	$("#main").append("<p>" + counter + "</p>");
 		}
-}
+	}
 });
